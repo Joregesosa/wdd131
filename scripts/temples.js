172445caa186d1edff3ff9e036497f1da0b09b27
Jorge Sosa <joregesosa@gmail.com>
2024-11-16 19:46:05 -0400

    add temples statically

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,36 +4,9 @@ const header = document.querySelector('header');
 const close_button = document.querySelector('#close_button');
 const menu_button = document.querySelector('#menu_button');
 const nav = document.querySelector('nav');
-const albums = document.querySelector('#album');
- 
-
 
 current_year.innerHTML = new Date().getFullYear();
 last_modification.innerHTML = new Date(document.lastModified).toLocaleString();
-
-fetch('assets/data/temples.json')
-    .then(response => response.json())
-    .then(data => data.forEach((temple) => displayTemple(temple)))
-    .catch(error => console.error(error.message));
-
-
-function displayTemple(temple) {
-    const { id, name, image } = temple;
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    const figcaption = document.createElement('figcaption');
-
-    img.src = image;
-    img.alt = name;
-
-    figcaption.textContent = name;
-    figure.id = id;
-    figure.classList.add('album_item');
-
-    figure.append(img, figcaption);
-
-    albums.appendChild(figure);
-}
 
 /* events */
 menu_button.addEventListener('click', function () {
@@ -46,7 +19,7 @@ close_button.addEventListener('click', function () {
 
 window.addEventListener('scroll', () => {
     const currentScroll = window.scrollY;
-    if (currentScroll > 100) {
+    if (currentScroll > 50) {
         header.classList.add('is-sticky');
     } else {
         header.classList.remove('is-sticky');
