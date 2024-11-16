34b204324d7ef823ff23ba84f1daac50a3ad8199
Jorge Sosa <joregesosa@gmail.com>
2024-11-15 22:46:37 -0400

    fix temple data route

--- scripts/temples.js
+++ scripts/temples.js
@@ -10,7 +10,7 @@ const albums = document.querySelector('#album');
 
 current_year.innerHTML = new Date().getFullYear();
 last_modification.innerHTML = new Date(document.lastModified).toLocaleString();
-fetch('../assets/data/temples.json')
+fetch('assets/data/temples.json')
     .then(response => response.json())
     .then(data => data.forEach((temple) => displayTemple(temple)))
     .catch(error => console.error(error.message));
